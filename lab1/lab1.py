93e92022f588dd4154e2c9b0582e8b81e0b5ea5d
Sasha Samoilov <samsiki50@gmail.com>
2022-02-16 00:11:18 +0300

    new lab

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,17 +1,17 @@
 from colorama import*
 init()
 import random, time, sys
-def film(x):###Функция для вывода фильма
-	print("Вами выбран фильм",x)
-def timeStart(a):###Функция для вывода временени
-	print("Время начала сеанса в",a,"часа(ов)")
+def film(x):
+	print("You have chosen the movie rocky",x)
+def timeStart(a):
+	print("Show starts at",a)
 def SetFilm(a,b,c,d):
-	print("Выберите фильм:\n1)",a,"\n2)",b,"\n3)",c,"\n4)",d)
+	print("Sekect a movie:\n1)",a,"\n2)",b,"\n3)",c,"\n4)",d)
 def TypeFilm(a):
-	print("Вы выбрали жанр",a)
+	print("You have chosen a genre of",a)
 def SetFilm2(a,b,c,d,q):
 	while True: 
-		q = int(input("\nФильм № "))
+		q = int(input("\nFilm № "))
 		if q == 1:
 			print(a)
 			q=a
@@ -25,7 +25,7 @@ def SetFilm2(a,b,c,d,q):
 			print(d)
 			q=d
 		else:
-			print("Ошибка ввода. Повторите попытку ввода")
+			print("Input error. Try again")
 		return q
 f1="Ghostbusters"
 f2="Back to the Future"
@@ -40,10 +40,10 @@ f10="Fight Club"
 f11="The Godfather"
 f12="Shine"
 f13="The Shawshank Redemption"
-tf1="Ужасы"
-tf2="Фантастика"
-tf3="Боевик"
-tf4="Драма"
+tf1="Horrors"
+tf2="Fiction"
+tf3="Action"
+tf4="Drama"
 t1=random.randint(5,9)
 t2=random.randint(10,14)
 t3=random.randint(15,19)
@@ -54,8 +54,8 @@ c=0
 animation = ("[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]")
 print(Fore.MAGENTA+"WELCOME TO THE KINONIA"+Fore.WHITE)
 while True: 
-	print("Выберите жанр фильма:\n1)",tf1,"\n2)",tf2,"\n3)",tf3,"\n4)",tf4)
-	type = int(input("\nЖанр "))
+	print("Choose a movie genre:\n1)",tf1,"\n2)",tf2,"\n3)",tf3,"\n4)",tf4)
+	type = int(input("\nGenre "))
 	if type == 1:
 		TypeFilm(tf1)
 		type=tf1
@@ -73,7 +73,7 @@ while True:
 		type=tf4
 		break
 	else:
-		print(Fore.RED+"Ошибка ввода. Повторите попыкту ввода"+Fore.WHITE)
+		print(Fore.RED+"Input error. Try again"+Fore.WHITE)
 if(type==tf1):
 	SetFilm(f4,f5,f6,f7)
 	c=SetFilm2(f4,f5,f6,f7,c)
@@ -87,15 +87,18 @@ elif(type==tf4):
 	SetFilm(f10,f11,f12,f13)
 	c=SetFilm2(f10,f11,f12,f13,c)
 else:
-	exit(0)
-	print("Ошибка программы")
+	pass
 if (c == f1,f5,f6,f7,f12):
 	price+=8.50
+else:
+	pass
 if (c == f2,f3,f4,f8,f10,f11,f13):
 	price+=10
-print("Выберите подходящее время сеанса:\n1)",t1,"\n2)",t2,"\n3)",t3,"\n4)",t4)
+else:
+	pass
+print("Select the appropriate session time:\n1)",t1,"\n2)",t2,"\n3)",t3,"\n4)",t4)
 while True:
-	tt = int(input("\nВремя №"))
+	tt = int(input("\nTime №"))
 	if tt == 1:
 		timeStart(t1)
 		tt=t1
@@ -115,9 +118,9 @@ while True:
 		price-=1.00
 		break
 	else:
-		print(Fore.RED+"Ошибка ввода. Повторите попыкту ввода"+Fore.WHITE)
-print("Выберите место в зале:")
-zal = [0] * 100##Создание списка
+		print(Fore.RED+"Input error. Try again"+Fore.WHITE)
+print("Chosse a seat in the hall:")
+zal = [0] * 100
 for i in range(len(zal) - 1):
     i = str(i + 1)
     i = int(i)
@@ -125,37 +128,37 @@ for i in range(len(zal) - 1):
 i=1
 while i < 100:
 	if i==1:
-		print(Fore.YELLOW+"1 Ряд"+Fore.WHITE)
+		print(Fore.YELLOW+"1 Row"+Fore.WHITE)
 	elif i==26:
-		print(Fore.YELLOW+"2 Ряд"+Fore.WHITE)
+		print(Fore.YELLOW+"2 Row"+Fore.WHITE)
 	elif i==51:
-		print(Fore.YELLOW+"3 Ряд"+Fore.WHITE)
+		print(Fore.YELLOW+"3 Row"+Fore.WHITE)
 	elif i==75:
-		print(Fore.YELLOW+"4 Ряд(VIP-Места)"+Fore.WHITE)
+		print(Fore.YELLOW+"4 Row(VIP place)"+Fore.WHITE)
 	else:
 		time.sleep(0);
 	if zal[i]==1:
-		print (Fore.RED + str(zal[i]),"=",i,"Место" + Fore.WHITE)
+		print (Fore.RED + str(zal[i]),"=",i,"Seat" + Fore.WHITE)
 		i=i+1
 	else:
-		print (Fore.GREEN + str(zal[i]),"=",i,"Место" + Fore.WHITE)
+		print (Fore.GREEN + str(zal[i]),"=",i,"Seat" + Fore.WHITE)
 		i=i+1
-print(Fore.GREEN+"\nЗеленый"+Fore.WHITE+" - место свободно")
-print(Fore.RED +"Красный"+Fore.WHITE+" - место занято")
+print(Fore.GREEN+"\nGreen"+Fore.WHITE+" - place is free")
+print(Fore.RED +"Red"+Fore.WHITE+" - place is occupided")
 while True:
-    z = int(input("\nВыберите свободное местов зале №"))
-    if zal[z] ==0:
-        print(Fore.GREEN+"Место свободно"+Fore.WHITE)
-        if z>=75:
-        	price+=15.00
-        break
-    else:
-        print(Fore.RED+"Данное место занято. Выберите место повторно."+Fore.WHITE)
+	z = int(input("\nChoose an free seat it the hall №"))
+	if zal[z] ==0:
+		print(Fore.GREEN+"Place is free"+Fore.WHITE)
+		if z>=75:
+			price+=15.00
+		break
+	else:
+		print(Fore.RED+"This place is occupied. Choose a location again."+Fore.WHITE)
 while True:
-	print("Узнать подробность о рассадке\nНужна помощь?\n1)Да\n2)Нет")
-	m=int(input("Введите вариант "))
+	print("Learn more about seating arrangements\nNeed help?\n1)Yes\n2)No")
+	m=int(input("Select an option"))
 	if (m == 1):
-		print("\n          Экран")
+		print("\n          Screen")
 		print("-------------------------\n")
 	p=0
 	for j in range(4):
@@ -165,31 +168,31 @@ while True:
 				print(Fore.GREEN+"#",end=""+Fore.WHITE)
 				continue
 			else:
-				time.sleep(0)
+				pass
 			print("*",end="")
-		print("<",j+1,"Ряд")
+		print("<",j+1,"Row")
 	break
 	if (m == 2):
 		break
 	else:
-		time.sleep(0)
-print("Хотите совершить онлайн покупку билета?\n1)Да\n2)Нет\nК опалате ",price)
+		pass
+print("Do you want to make an online ticked purchase?\n1)Yes\n2)No\nTo be paid ",price)
 while True:
-	nub=int(input("Выберите вариант "))
+	nub=int(input("Select an option"))
 	if ( nub == 1):
 		while True:
-			card = input("Введите номер карты\n")
+			card = input("Enter the card number\n")
 			q = 0
 			counter = 0
 			for i in card:
-			    if i.isdigit():
-			        counter+=1
-			    else:
-			    	time.sleep(0)
+				if i.isdigit():
+					counter+=1
+				else:
+					pass
 			if (counter == 16):
 				break
 			else:
-				print("Ошибка ввода. Повторите попытку")
+				print("Error input. Try again")
 		q=int(card)
 		q=q//1000000000000000
 		if ( q == 3 ):
@@ -201,16 +204,15 @@ while True:
 		elif ( q == 6):
 			print(modelcard ['6'])
 		else:
-			print("Ошибка ввода карты. Перезапустите программу.")
-			exit(0)
+			pass
 		while True:
-			data = list(map(int,input('Введите срок действия карты через пробел ').split()))
+			data = list(map(int,input('Enter the card expiration date separeted by a space ').split()))
 			if (((data[0] > 0) | (data[0] <= 12))&(data[1] >= 21)):
 				break
 			else:
-				print("Карта просроченна. Повторите попытку ввода")
+				print("Card expired. Try again")
 		if nub == 1:
-			print("Генерая PASS ID")
+			print("PASS generation")
 			for i in range(len(animation)):
 				time.sleep(0.4)
 				sys.stdout.write("\r" + animation[i % len(animation)])
@@ -220,13 +222,13 @@ while True:
 				pas = pas + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ$@%&*'))
 		else:
 			time.sleep(0.00001)
-		print(Fore.GREEN+"\nID-pass готов"+Fore.WHITE)
-		print("Ваш ID-pass ",pas)
-		print("Не показывайте свой PASS ID 3-лицам")
-		price = "Оплачено"
+		print(Fore.GREEN+"\nPass has been created"+Fore.WHITE)
+		print("Your PASS ",pas)
+		print("Do not show your PASS to anyone's faces")
+		price = "Paid"
 		break
 	elif ( nub == 2):
 		break
 	else:
-		print("Ошибка ввода")
-print("\nВы выбрали фильм:",c,"\nЖанр:",type,"\nНачало сеанса в",tt,"часа(ов)\nВаше место №",z,"\nК опате:",price,"\nПриятного просмотра!")
+		print("Error ")
+print("\nYou have chosen a movie:",c,"\nGenre of:",type,"\nSession start in ",tt,"\nYour seat №",z,"\nto be paid:",price,"\nNice view!")
